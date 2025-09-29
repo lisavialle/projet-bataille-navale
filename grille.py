8d993bd362720ef1ff9591799c753f47b46334b4
lisavialle <lisa.vialle@centrale-med.fr>
2025-09-29 15:59:40 +0200

    Feature: Classe Grille avec constructeur et __str__ + tests

--- grille.py
+++ grille.py
@@ -1,3 +1,25 @@
-class Grille :
-    def __init__(self):
-        pass
+class Grille:
+    """
+    Classe Grille pour le jeu Bataille Navale.
+    Attributs :
+        - lignes : nombre de lignes
+        - colonnes : nombre de colonnes
+        - vide : caractère pour une case vide ('∿')
+        - cases : liste de listes représentant la grille
+    Méthodes :
+        - __str__ : affiche la grille sous forme de texte
+    """
+
+    def __init__(self, lignes, colonnes):
+        self.lignes = lignes
+        self.colonnes = colonnes
+        self.vide = '∿'
+        # création de la grille : liste de listes initialisée avec le caractère vide
+        self.cases = [[self.vide for _ in range(colonnes)] for _ in range(lignes)]
+
+    def __str__(self):
+        """
+        Retourne la grille sous forme de texte pour affichage.
+        Chaque ligne est une chaîne, les lignes sont séparées par des sauts de ligne.
+        """
+        return "\n".join("".join(ligne) for ligne in self.cases)
--- tests/test_grille.py
+++ tests/test_grille.py
@@ -3,9 +3,17 @@ from grille import Grille
 
 def test_init():
     """
-    Test de création d'un objet Grille.
-    Vérifie que l'objet est bien de type Grille.
+    Teste que l'on peut créer une grille et que ses dimensions sont correctes.
     """
-    g = Grille()
+    g = Grille(5, 8)
     assert isinstance(g, Grille)
-    
+    assert len(g.cases) == 5          # 5 lignes
+    assert len(g.cases[0]) == 8       # 8 colonnes
+
+def test_str():
+    """
+    Teste que l'affichage __str__ fonctionne correctement
+    """
+    g = Grille(2, 3)
+    expected = "∿∿∿\n∿∿∿"
+    assert str(g) == expected
